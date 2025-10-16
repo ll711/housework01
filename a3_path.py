9cb3b583b01f0de9d827def0fc0d4baad8a6aa00
Chenrong Li <158499143+ll711@users.noreply.github.com>
2025-10-16 18:41:22 +0100

    Add files via upload

---
+++ a3_path.py
@@ -0,0 +1,24 @@
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+Includes a State class for Task 1
+
+@author: B20 (100528137 and 100531086)
+@date: 11/10/2025
+
+"""
+# genet ic algorithm
+class Agent:
+    def __init__(self):
+        pass
+    def genetic_algorithm(self,population,fitness_function,mutation_rate,generations):
+        pass
+    def crossover(self,parent1,parent2):
+        pass
+    def mutate(self,individual,mutation_rate):
+        pass
+    def select_parents(self,population,fitness_function):
+        pass
+    def create_initial_population(self,size):
+        pass
---
+++ a4_path.py
@@ -0,0 +1,19 @@
+"""
+Hinger Project
+Coursework 001 for: CMP-6058A Artificial Intelligence
+
+Includes a State class for Task 1
+
+@author: B20 (100528137 and 100531086)
+@date: 11/10/2025
+
+"""
+#main game loop
+# load a1, a2, a3 files and give a GUI graphical interface to choose which moode to
+# run(matichine VS machine, human VS machine)
+#in the GUI, show a star path, and import the path from a3 file
+# show the path on the GUI and give who need to active ahead
+from a1_state import State
+from a3_path import Agent
+
+
